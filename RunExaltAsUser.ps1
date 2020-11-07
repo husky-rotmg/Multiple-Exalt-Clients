5676f92178d1e0e3347b5b3bca7625405bb42f1f
kerr-andrew <38387144+kerr-andrew@users.noreply.github.com>
2020-11-07 15:25:11 -0800

    Fixed a lot of the update functionality.
    You may need to re-download if you're on version 1.6 or below.

--- RunExaltAsUser.ps1
+++ RunExaltAsUser.ps1
@@ -1,5 +1,5 @@
-﻿
-$running_vers = [Version]"1.6"
+﻿$running_vers = [Version]"1.7"
+
 
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName PresentationFramework
@@ -175,8 +175,8 @@ Function RunExaltFromList
     $chkRemember = New-Object System.Windows.Forms.CheckBox
     $chkRemember.Location = New-Object System.Drawing.Point(10, 290)
     $chkRemember.Width = 260
-    if ([string]::IsNullOrWhiteSpace($data.Settings.remember)) {
-        $data.Settings.remember = "true"
+    if ($data.Settings.remember -eq $null) {
+        $data.Settings.remember = "True"
         Save-Settings -Data $data -File $File
     }
 
@@ -360,39 +360,9 @@ Function Save-Settings
     Set-Content -Path $File -Value $($Data | New-IniContent)
 }
 
+# https://gist.github.com/beruic/1be71ae570646bca40734280ea357e3c
 function Get-IniFile 
 {
-    <#
-    .SYNOPSIS
-    Read an ini file.
-    
-    .DESCRIPTION
-    Reads an ini file into a hash table of sections with keys and values.
-    
-    .PARAMETER filePath
-    The path to the INI file.
-    
-    .PARAMETER anonymous
-    The section name to use for the anonymous section (keys that come before any section declaration).
-    
-    .PARAMETER comments
-    Enables saving of comments to a comment section in the resulting hash table.
-    The comments for each section will be stored in a section that has the same name as the section of its origin, but has the comment suffix appended.
-    Comments will be keyed with the comment key prefix and a sequence number for the comment. The sequence number is reset for every section.
-    
-    .PARAMETER commentsSectionsSuffix
-    The suffix for comment sections. The default value is an underscore ('_').
-    .PARAMETER commentsKeyPrefix
-    The prefix for comment keys. The default value is 'Comment'.
-    
-    .EXAMPLE
-    Get-IniFile /path/to/my/inifile.ini
-    
-    .NOTES
-    The resulting hash table has the form [sectionName->sectionContent], where sectionName is a string and sectionContent is a hash table of the form [key->value] where both are strings.
-    This function is largely copied from https://stackoverflow.com/a/43697842/1031534. An improved version has since been pulished at https://gist.github.com/beruic/1be71ae570646bca40734280ea357e3c.
-    #>
-    
     param(
         [parameter(Mandatory = $true)] [string] $filePath,
         [string] $anonymous = 'NoSection',
--- update.ps1
+++ update.ps1
@@ -1,7 +1,6 @@
 ﻿Function Update-MEC
 {
     param($Current, $Latest, $File)
-    try {
     Write-Host "Removing old update files..."
     Remove-Item -Path "update.zip" -Force -ErrorAction Ignore
     Remove-Item -Path "update\" -Force -Recurse -ErrorAction Ignore
@@ -18,38 +17,22 @@
     Write-Host "Updating files..."
     Get-ChildItem -Path $(Get-ChildItem -Path "update\")[0].FullName |
     Foreach-Object {
-        $file = $_
-        if (@("accounts.ini") -contains $file.Name)
+        if ($_.Name -eq $File)
         {
             return
         }
-        Write-Host "Updating $file..."
-        Move-Item -Path $file.FullName -Destination $file.Name -Force -ErrorAction Inquire
+        Write-Host "Updating $_..."
+        Move-Item -Path $_.FullName -Destination $_.Name -Force -ErrorAction Ignore
     }
 
     Write-Host "Removing update archive..."
-    Remove-Item -Path "update\" -Force -Recurse -ErrorAction Stop
-
-    . .\RunExaltAsUser.ps1
-
-    if ($running_vers -eq [Version]$Latest)
-    {
-        Write-Host "Successfully Updated"
-        Add-Type -AssemblyName System.Windows.Forms
-        [System.Windows.Forms.MessageBox]::Show("Updated Multiple-Exalt-Clients from version $Current to $Latest.", "Updated Successfully", "OK", "info")
+    Remove-Item -Path "update\" -Force -Recurse -ErrorAction Ignore
     
-    } else {
-        Write-Host "There was a problem updating the clients..."
-        Write-Host "Manually download the update from https://github.com/husky-rotmg/multiple-exalt-clients/archive/v$Latest.zip"
-        [System.Windows.Forms.MessageBox]::Show("There was a problem updating the clients... Manually download the update from https://github.com/husky-rotmg/multiple-exalt-clients/archive/v$Latest.zip", "Update Failed", "OK", "Error")
-    }
-    if ($File -eq $null)
-    {
-        $File = "accounts.ini"
-    }
-    RunExalt -File $File 
-    } catch {
-        [System.Windows.MessageBox]::Show("There was an issue updating the packages.`n$_", "Update Error", "OK", "Error")
-        Exit 0
-    }
+    Add-Type -AssemblyName System.Windows.Forms
+
+    Write-Host "Successfully Updated"
+    Add-Type -AssemblyName System.Windows.Forms
+    [System.Windows.Forms.MessageBox]::Show("Updated Multiple-Exalt-Clients from version $Current to $Latest.", "Updated Successfully", "OK", "info")
+
+    .\RunExaltAM.cmd -File $File
 }
